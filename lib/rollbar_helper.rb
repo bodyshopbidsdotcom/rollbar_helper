d3cfc966d4404788197dd8ea8066777160ce67e5
Santiago Herrera <santiago.herrera@snapsheet.me>
2017-02-09 00:47:59 -0600

    adding error method

--- lib/rollbar_helper.rb
+++ lib/rollbar_helper.rb
@@ -1,11 +1,24 @@
 class RollbarHelper
-  VERSION = "0.0.0"
+  VERSION = "0.0.1"
 
   class << self
-    def sayit
-      msg = 'holaaaa!'
-      puts msg
-      return msg
+
+    def error(obj, fingerprint: nil)
+      e = nil
+
+      if obj.is_a?(Exception)
+        e = obj
+      else
+        e = StandardError.new(obj.to_s)
+        e.set_backtrace(caller)
+      end
+
+      if fingerprint.present?
+        Rollbar.scope(:fingerprint => fingerprint).error(e)
+      else
+        Rollbar.error(e)
+      end
     end
+
   end
 end
