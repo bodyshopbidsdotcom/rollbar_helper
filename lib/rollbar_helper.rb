370e402a95f213340c2e7d562b712e13ed887c8a
Santiago Herrera <santiago.herrera@snapsheet.me>
2017-02-09 01:27:46 -0600

    adding configuration

--- lib/rollbar_helper.rb
+++ lib/rollbar_helper.rb
@@ -1,5 +1,5 @@
 class RollbarHelper
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 
   class << self
 
@@ -17,6 +17,12 @@ class RollbarHelper
         Rollbar.scope(:fingerprint => fingerprint).error(e)
       else
         Rollbar.error(e)
+      end
+    end
+
+    def configure
+      Rollbar.configure do |config|
+        yield(config)
       end
     end
 
