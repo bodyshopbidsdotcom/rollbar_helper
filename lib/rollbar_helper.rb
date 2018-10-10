0c720c900d305e877bc21019fdf6f9feabff38bf
Nick Stanish <nickstanish@gmail.com>
2018-10-10 11:32:26 -0500

    Add additional log levels and tests

--- lib/rollbar_helper.rb
+++ lib/rollbar_helper.rb
@@ -1,24 +1,57 @@
+require 'rollbar'
+
 class RollbarHelper
   VERSION = "0.2.0"
+  LEVELS = [
+    :critical,
+    :debug,
+    :error,
+    :info,
+    :warning,
+  ]
 
   class << self
+    def critical(obj, fingerprint: nil, **data)
+      log(:critical, obj, :callee => caller, fingerprint: fingerprint, **data)
+    end
+
+    def debug(obj, fingerprint: nil, **data)
+      log(:debug, obj, :callee => caller, fingerprint: fingerprint, **data)
+    end
 
     def error(obj, fingerprint: nil, **data)
+      log(:error, obj, :callee => caller, fingerprint: fingerprint, **data)
+    end
+
+    def info(obj, fingerprint: nil, **data)
+      log(:info, obj, :callee => caller, fingerprint: fingerprint, **data)
+    end
+
+    def warn(obj, fingerprint: nil, **data)
+      log(:warning, obj, :callee => caller, fingerprint: fingerprint, **data)
+    end
+
+    def warning(obj, fingerprint: nil, **data)
+      log(:warning, obj, :callee => caller, fingerprint: fingerprint, **data)
+    end
+
+    def log(level, obj, callee: caller, fingerprint: nil, **data)
+      level = level.to_sym
+      raise ArgumentError, 'Log level is not supported' unless LEVELS.include?(level)
       e = nil
 
       if obj.is_a?(Exception)
         e = obj
       else
         e = StandardError.new(obj.to_s)
-        e.set_backtrace(caller)
+        e.set_backtrace(callee)
       end
 
-      if fingerprint.present?
-        Rollbar.scope(:fingerprint => fingerprint).error(e, data)
+      unless fingerprint.nil?
+        ::Rollbar.scope(:fingerprint => fingerprint).send(level, e, data)
       else
-        Rollbar.error(e, data)
+        ::Rollbar.send(level, e, data)
       end
     end
-
   end
 end
--- spec/rollbar_helper_spec.rb
+++ spec/rollbar_helper_spec.rb
@@ -1,7 +1,50 @@
 require "spec_helper"
 
+RSpec::Matchers.define :caller_backtrace do |expected|
+  match do |actual|
+    actual.backtrace[0].include?(expected)
+  end
+end
+
 RSpec.describe RollbarHelper do
   it "has a version number" do
     expect(RollbarHelper::VERSION).not_to be nil
+  end
+
+  it 'responds to log level methods' do
+    expect(RollbarHelper.respond_to?(:critical)).to eq(true)
+    expect(RollbarHelper.respond_to?(:debug)).to eq(true)
+    expect(RollbarHelper.respond_to?(:error)).to eq(true)
+    expect(RollbarHelper.respond_to?(:info)).to eq(true)
+    expect(RollbarHelper.respond_to?(:warning)).to eq(true)
+  end
+
+  it 'wraps rollbar method with error' do
+    expect(Rollbar).to receive(:error).with(instance_of(StandardError), :data => true)
+    RollbarHelper.error('Oops!', :data => true)
+  end
+
+  it 'uses callers backtrace' do
+    expect(Rollbar).to receive(:error).with(caller_backtrace(__FILE__), :data => true)
+    RollbarHelper.error('Oops!', :data => true)
+  end
+
+  it 'scopes with fingerprint' do
+    expect(Rollbar).to receive(:scope).with(:fingerprint => 'processor').and_return(Rollbar)
+    expect(Rollbar).to receive(:error).with(caller_backtrace(__FILE__), :data => true)
+    RollbarHelper.error('Oops!', :fingerprint => 'processor', :data => true)
+  end
+
+  describe '#log' do
+    it 'uses callers backtrace' do
+      expect(Rollbar).to receive(:error).with(caller_backtrace(__FILE__), :data => true)
+      RollbarHelper.log(:error, 'Oops!', :data => true)
+    end
+
+    it 'raises on unsupported log levels' do
+      expect {
+        RollbarHelper.log(:silly, 'Oops!', :data => true)
+      }.to raise_error(ArgumentError, 'Log level is not supported')
+    end
   end
 end
